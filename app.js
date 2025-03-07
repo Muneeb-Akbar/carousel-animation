40cc115762afb8c23eedb89cc9d54877bffebe8d
Muneeb-Akbar <muneeb.akar09@gmail.com>
2025-03-07 19:44:20 +0500

    add annimation on next button

---
+++ app.js
@@ -0,0 +1,24 @@
+let nextDom = document.getElementById("next");
+let prevDom = document.getElementById("prev");
+let carouselDom = document.querySelector(".carousel");
+let carouselList = document.querySelector(".carousel .list");
+let thumbnailDom = document.querySelector(".carousel .thumbnail");
+
+nextDom.onclick = function () {
+  showSlider("next");
+};
+function showSlider(type) {
+  let itemSlider = document.querySelectorAll(".carousel .list .item");
+  let itemThumbnail = document.querySelectorAll(".carousel .thumbnail .item");
+  if (type === "next") {
+    carouselList.appendChild(itemSlider[0]);
+    thumbnailDom.appendChild(itemThumbnail[0]);
+    carouselDom.classList.add("next");
+    console.log(carouselList)
+  }else{
+    console.log("hello")
+    carouselList.prepend(itemSlider[itemSlider.length - 1]);
+    thumbnailDom.prepend(itemThumbnail[itemThumbnail.length - 1]);
+    carouselDom.classList.add("prev");
+  }
+}
